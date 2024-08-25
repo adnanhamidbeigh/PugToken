15c1e6f7612e40050c4e86799f406ef515eb628f
adnanhamidbeigh <adnanhamidbeg@gmail.com>
2024-08-25 15:44:29 +0000

    v1

--- script/Constants.s.sol
+++ script/Constants.s.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-
 abstract contract Constants {
     uint256 public constant INITIAL_SUPPLY = 1e7 ether;
 }
--- src/PugToken.sol
+++ src/PugToken.sol
@@ -3,15 +3,17 @@ pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 contract PugToken is ERC20, Ownable {
     constructor(uint256 initalSupply) ERC20("PugToken", "PUG") Ownable(msg.sender) {
         _mint(msg.sender, initalSupply);
     }
 
-    function mint(uint256 amount) onlyOwner public {
+    function mint(uint256 amount) public onlyOwner {
         _mint(msg.sender, amount);
     }
-    function burn(uint256 amount) onlyOwner public {
+
+    function burn(uint256 amount) public onlyOwner {
         _burn(msg.sender, amount);
     }
 }
